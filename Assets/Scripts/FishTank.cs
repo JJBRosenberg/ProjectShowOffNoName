01784872434000f9a7e517e27463abff4162ab98
Doombringer <adragnea5@gmail.com>
2021-07-09 10:44:45 +0300

    Added fish tank text box
    And tried to put the code for the safes and metal box spawning

--- Assets/Scripts/FishTank.cs
+++ Assets/Scripts/FishTank.cs
@@ -2,27 +2,62 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class FishTank : MonoBehaviour
 {
 
     [SerializeField] private bool isInRange;
     [SerializeField] private Item item;
     [SerializeField] private Item itemBack;
+    public bool inTrigger;
     [SerializeField] private GameObject coinToDestory;
     public ItemContainer itemContainer;
+    [SerializeField] bool close;
 
-    private void Update()
+    private void Start()
     {
-        LookUnder(itemContainer);
+        close = true;
     }
 
-    public void LookUnder(IItemContainer itemContainer)
+    //Checks if player entered 
+    private void OnTriggerEnter(Collider other)
     {
-        if (itemContainer.ContainsItem(item) && isInRange && Input.GetKeyDown(KeyCode.E))
+        if (other.gameObject.tag == "Player")
         {
-            itemContainer.AddItem(itemBack);
-            itemContainer.RemoveItem(item);
-            Destroy(coinToDestory);
+            isInRange = true;
+        }
+    }
+    //Checks if player exited
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            isInRange = false;
+        }
+    }
+    private void Update()
+    {
+        LookUnder();
+    }
+
+
+    public void LookUnder()
+    {
+        if (isInRange && Input.GetKeyDown(KeyCode.E))
+        {
+            if (itemContainer.ContainsItem(item))
+            {
+
+                itemContainer.AddItem(itemBack);
+                itemContainer.RemoveItem(item);
+                Destroy(coinToDestory);
+                GameFeedback.Instance.SetText("Coin half taken!");
+
+            }
+            else
+            {
+                GameFeedback.Instance.SetText("There is something inside. Press E to interact with the correct item.");
+            }
         }
     }
 }
